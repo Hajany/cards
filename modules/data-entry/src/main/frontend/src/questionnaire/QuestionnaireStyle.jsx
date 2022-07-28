9611657837210233ae5394127703b49b64dbc33a
veronikaslc <veronika.koltunova@sickkids.ca>
2022-07-28 05:04:32 -0400

    CARDS-1761: Adapt React Date / Time pickers Proof of concept for the TimePicker component

--- modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
+++ modules/data-entry/src/main/frontend/src/questionnaire/QuestionnaireStyle.jsx
@@ -687,6 +687,9 @@ const questionnaireStyle = theme => ({
         paddingTop: theme.spacing(4),
       },
     },
+    datePickerError: {
+        paddingBottom: theme.spacing(2),
+    },
 });
 
 export default questionnaireStyle;
