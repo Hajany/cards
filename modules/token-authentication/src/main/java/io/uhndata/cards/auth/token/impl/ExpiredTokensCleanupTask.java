d7692fa4ea3696138df16c852b31da4fc31c9cbf
Sergiu Dumitriu <sergiu@phenotips.org>
2023-02-27 10:43:06 -0500

    CARDS-2078: Extract a CardsToken interface to publicly expose some details of the token implementation

--- modules/token-authentication/src/main/java/io/uhndata/cards/auth/token/impl/ExpiredTokensCleanupTask.java
+++ modules/token-authentication/src/main/java/io/uhndata/cards/auth/token/impl/ExpiredTokensCleanupTask.java
@@ -32,6 +32,8 @@ import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.uhndata.cards.auth.token.CardsToken;
+
 public class ExpiredTokensCleanupTask implements Runnable
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExpiredTokensCleanupTask.class);
@@ -48,7 +50,7 @@ public class ExpiredTokensCleanupTask implements Runnable
     {
         try (ResourceResolver resolver = this.rrf.getServiceResourceResolver(null)) {
             final Iterator<Resource> resources = resolver.findResources("SELECT * FROM [cards:Token] WHERE ["
-                + CardsTokenImpl.TOKEN_ATTRIBUTE_EXPIRY + "] < '" + ZonedDateTime.now() + "'", Query.JCR_SQL2);
+                + CardsToken.TOKEN_ATTRIBUTE_EXPIRY + "] < '" + ZonedDateTime.now() + "'", Query.JCR_SQL2);
             resources.forEachRemaining(token -> {
                 try {
                     resolver.delete(token);
